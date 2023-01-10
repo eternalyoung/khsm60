648be820ee2f55581bc7f056f1f71e751fb31edb
andrew <kondratev-28@mail.ru>
2023-01-11 03:01:26 +0700

    2 + 2 fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,13 +9,7 @@ RSpec.describe Question, type: :model do
     it { should_not allow_value(15).for(:level) }
     it { should allow_value(14).for(:level) }
 
-    subject do
-      Question.new(text: 'sample', level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1')
-    end
-
-    it do
-      question = Question.create!(text: 'sample_but_another', level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1')
-      expect(question).to validate_uniqueness_of(:text)
-    end
+    subject { FactoryGirl.create(:question) }
+    it { should validate_uniqueness_of :text}
   end
 end
