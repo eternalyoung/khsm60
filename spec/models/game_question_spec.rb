ba23b01477726097af49813d3a2aba66dbd07976
andrew <kondratev-28@mail.ru>
2023-01-14 04:03:42 +0700

    help_hash fifty_fifty friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -35,9 +35,64 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
-  context "#correct_answer_key" do
+  describe "#correct_answer_key" do
     it 'returns correct answer key' do
       expect(game_question.correct_answer_key).to eq('b')
     end
-  end  
+  end
+
+  describe "#help_hash" do
+    it 'empty on create' do
+      expect(game_question.help_hash).to eq({})
+    end
+    context "with some saved pairs" do
+      before do
+        game_question.help_hash[:key_test1] = 'test1'
+        game_question.help_hash['key_test2'] = 'test2'
+        game_question.save
+      end
+
+      it 'returns correct pairs' do
+        expect(game_question.help_hash).to eq({key_test1: 'test1', 'key_test2' => 'test2'})
+      end
+    end
+  end
+
+  describe "#fifty_fifty" do
+    it 'help_hash doesent have fifty_fifty key' do
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+    end
+    context "with used fifty_fifty" do
+      before { game_question.add_fifty_fifty }
+
+      it 'writes fifty_fifty key' do
+        expect(game_question.help_hash).to include(:fifty_fifty)
+      end
+
+      it 'has correct answer key' do
+        expect(game_question.help_hash[:fifty_fifty]).to include('b')
+      end
+
+      it 'has 2 variants' do
+        expect(game_question.help_hash[:fifty_fifty].size).to eq 2
+      end
+    end
+  end
+
+  describe "#friend_call" do
+    it 'help_hash doesent have friend_call key' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+    end
+    context "with used friend_call" do
+      before { game_question.add_friend_call }
+
+      it 'writes friend_call key to help_hash' do
+        expect(game_question.help_hash).to include(:friend_call)
+      end
+
+      it 'has answer letter' do
+        expect(game_question.help_hash[:friend_call]).to match(/.*[ABCD]/)
+      end
+    end
+  end
 end
